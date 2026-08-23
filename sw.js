827aa345f33374f0d97ddc5027ea022e9a06892e
nbc-nu <nbc.nuno@gmail.com>
2026-08-23 18:03:23 -0300

    Simplify service worker by removing cache handling
    Removed caching logic and simplified service worker events.

--- sw.js
+++ sw.js
@@ -1,23 +1,4 @@
-const CACHE_NAME = 'v10-cache';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './image-192.svg',
-  './image-512.svg'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS_TO_CACHE)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+// Nenhum cache, nenhuma interceptação. Vazio.
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => self.clients.claim());
+self.addEventListener('fetch', (e) => {});
