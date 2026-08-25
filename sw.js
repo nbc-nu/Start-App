aace35f352166149b63158c91749ebd5088c5b18
nbc-nu <nbc.nuno@gmail.com>
2026-08-25 18:19:05 -0300

    Implement caching in service worker

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
+const CACHE_NAME = 'b-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
